49cdcc314c96890cd749a17cce3fd005421161db
Paul Water <p.r.water@setbased.nl>
2018-07-19 08:25:53 +0200

    Use strict types.

--- test/CoreSessionTest.php
+++ test/CoreSessionTest.php
@@ -1,5 +1,6 @@
 <?php
-//----------------------------------------------------------------------------------------------------------------------
+declare(strict_types=1);
+
 namespace SetBased\Abc\Session\Test;
 
 use PHPUnit\Framework\TestCase;
--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 mb_internal_encoding('UTF-8');
 error_reporting(E_ALL);
 date_default_timezone_set('Europe/Amsterdam');
